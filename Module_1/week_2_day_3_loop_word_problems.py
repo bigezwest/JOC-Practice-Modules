65d07c024be3df38aaa05cf715f52ac7e6ef6bd2
Thomas DAngelo <tommy@languagetribe.com>
2023-07-21 15:39:05 -0500

    Initial Commit, Week 2, Day 3 For Loops

---
+++ Module_1/week_2_day_3_loop_word_problems.py
@@ -0,0 +1,28 @@
+# Writing Loop Word Problems
+# Thomas D'Angelo
+
+# 1
+for i in range(10, 26, 5):
+    print(i, end=" ")
+print()
+
+# 2
+print("------------------\n")
+for i in range(-3, 22, 3):
+    if i < 21:
+        print(i, ",", end=" ", sep='')
+    else:
+        print(i, end=" ", sep='')
+print()
+
+''' Alternative Solution
+print("------------------\n")
+for i in range(-3, 21, 3):
+    print(i, end=", ")
+print(i + 3)
+'''
+# 3
+# See avg.py
+print("------------------\n")
+print("See day_3_avg.py for question 3.")
+
