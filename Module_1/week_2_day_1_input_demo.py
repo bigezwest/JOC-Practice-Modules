da88743049901cc3a5cfd2fa3e1afc2240f7a373
Thomas DAngelo <tommy@languagetribe.com>
2023-07-21 15:57:51 -0500

    Updated a file reference for avg.py

---
+++ Module_1/week_2_day_1_input_demo.py
@@ -0,0 +1,6 @@
+color = input("What is your favorite color? ")
+print("Cool!  My favorite color is _light_", color, "!")
+
+q2 = "On a scale of 1 to 10, how much do you like Urban Fire? "
+rating = float(input(q2))
+print("Cool! I like it", rating*1.8, "!")
