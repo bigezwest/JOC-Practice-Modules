89bdc6c0ef9d64d3f89a7719e8cf236847fd4f44
Thomas DAngelo <tommy@languagetribe.com>
2023-07-21 16:14:26 -0500

    Initial Commit

---
+++ Module_1/week_2_day_2_fun.py
@@ -0,0 +1,15 @@
+import math
+
+# Math fun
+print("The square root of 25 is \t\t", math.sqrt(25))
+print("The log10 of 100 is \t\t\t", math.log10(100))
+print("The absolute value of -5 is \t", abs(-5))
+
+phrase = "find your Yoda"
+print("-------------")
+print(phrase.upper())
+print(phrase.title())
+print(phrase.count("o"))
+print(phrase.lower().islower())
+print(phrase.isdecimal())
+print("---------------")
