721d84fd9f3899d393a585a233d3b68a8f4eaeeb
Thomas DAngelo <tommy@languagetribe.com>
2023-07-21 16:07:39 -0500

    Initial Commit

---
+++ Module_1/week_2_day_2_movie_rating.py
@@ -0,0 +1,14 @@
+# Thomas D'Angelo
+# Get a movie rating from the user.
+
+print("--------------------------------------------------------")
+print("My favorite movie is History of the World, Part I")
+print('')
+print("\tHow would you rate this movie? ")
+user_rating = int(input('\tEnter a number ( 1 - 5): '))
+
+my_rating = 5
+rating_difference = my_rating - user_rating
+
+print("\nThe difference in our ratings is ", rating_difference)
+print("--------------------------------------------------------")
