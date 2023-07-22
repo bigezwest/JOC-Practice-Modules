f569d7e1169e52d9e0d5f83bf9a83c4f37f01b91
Thomas DAngelo <tommy@languagetribe.com>
2023-07-22 10:18:02 -0500

    Initial Commit and Push.  Created absolute_difference(), & test case.

---
+++ Module_1/week_3_day_2_absolute_difference.py
@@ -0,0 +1,9 @@
+# Write a function, absolute_difference, that takes two numbers as parameters and returns
+# their absolute difference.
+
+def absolute_difference(x, y):
+    return abs(x - y)
+
+
+
+print(absolute_difference(5, 10))
