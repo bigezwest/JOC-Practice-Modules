cf3b42a8a8fbbee978120aebf3fb7ae210133a17
Thomas DAngelo <tommy@languagetribe.com>
2023-07-22 10:01:10 -0500

    Initial Commit and Push. Basic functionality.

---
+++ Module_1/week_3_day_2_pyramid.py
@@ -0,0 +1,6 @@
+# Write a function, pyramid, that takes a single character and a number as parameters and displays an
+# ASCII art pyramid to the screen with number rows:
+
+
+for i in range(2):
+    print("*" * (i + 1))
