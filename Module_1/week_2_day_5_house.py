3179c96ae82db4d817303f0309a771eee14ba2ed
Thomas DAngelo <tommy@languagetribe.com>
2023-07-21 20:38:20 -0500

    Initial Commit.

---
+++ Module_1/week_2_day_5_house.py
@@ -0,0 +1,41 @@
+import turtle
+
+num_sides = 4
+total_degrees = 360
+
+
+# Draw a square
+def square(length):
+    for side in range(num_sides):
+        turtle.forward(length)
+        turtle.right(total_degrees / num_sides)
+
+
+# Draw a triangle
+def triangle(length):
+    t_num_sides = num_sides - 1
+    for side in range(t_num_sides):
+        turtle.forward(length)
+        turtle.left(total_degrees / t_num_sides)
+
+
+# Create a space, 10 more than size of the length (used in triangle, and square)
+def turtle_shift(length):
+    turtle.penup()
+    turtle.backward(length)
+    turtle.pendown()
+
+
+def main():
+    size = 100                      # size of one side
+    for i in range(5):              # draw 5 times
+        triangle(size)
+        square(size)
+        turtle_shift(size + 10)     # create a space for the next house
+        size *= .75                 # size of next line
+
+    turtle.Screen().exitonclick()
+
+
+main()
+
