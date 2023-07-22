03d09415e9eabc6ff972237d0da64aad8abdb4f7
Thomas DAngelo <tommy@languagetribe.com>
2023-07-22 10:21:46 -0500

    Added main, 2 test cases, Comments.

--- Module_1/week_3_day_2_absolute_difference.py
+++ Module_1/week_3_day_2_absolute_difference.py
@@ -1,9 +1,16 @@
+# Thomas D'Angelo
 # Write a function, absolute_difference, that takes two numbers as parameters and returns
 # their absolute difference.
 
+# Return the absolute difference of 2 numbers
 def absolute_difference(x, y):
     return abs(x - y)
 
 
+def main():
+    print(absolute_difference(5, 10))           # Test with negative difference
+    print(absolute_difference(10, 5))           # Test with positive difference
+    print(absolute_difference(200, -200))       # Test subtracting a negative.  (adding)
 
-print(absolute_difference(5, 10))
+
+main()
