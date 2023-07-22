636b3c44eb49c126e4c755b033cfc3c90d7a1d13
Thomas DAngelo <tommy@languagetribe.com>
2023-07-22 11:04:54 -0500

    Formatting of long lines.

--- Module_1/week_3_day_2_absolute_difference.py
+++ Module_1/week_3_day_2_absolute_difference.py
@@ -8,9 +8,12 @@ def absolute_difference(x, y):
 
 
 def main():
-    print("The absolute difference of \t5 - 10 = ", absolute_difference(5, 10))           # Test with negative difference
-    print("The absolute difference of \t10 - 5 = ", absolute_difference(10, 5))           # Test with positive difference
-    print("The absolute difference of \t200 -(-200) = ", absolute_difference(200, -200))       # Test subtracting a negative.  (adding)
+    print("The absolute difference of \t5 - 10 = ",
+          absolute_difference(5, 10))                       # Test with negative difference
+    print("The absolute difference of \t10 - 5 = ",
+          absolute_difference(10, 5))                       # Test with positive difference
+    print("The absolute difference of \t200 -(-200) = ",
+          absolute_difference(200, -200))                   # Test subtracting a negative.  (adding)
 
 
 main()
