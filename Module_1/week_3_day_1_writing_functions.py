5441e977ea1ed963e2b975e630eec9b5e0396f9d
Thomas DAngelo <tommy@languagetribe.com>
2023-07-21 21:46:11 -0500

    Added multiply function.  Updated Comments

--- Module_1/week_3_day_1_writing_functions.py
+++ Module_1/week_3_day_1_writing_functions.py
@@ -1,7 +1,15 @@
-# Thomas D'Angelo
-# 1. Write a function, add, that takes two numbers as parameters and
-# returns their sum.
+"""
+Thomas D'Angelo
+    1. Write a function, add, that takes two numbers as parameters and returns their sum.
+    2. Write a function, multiply, that takes two numbers as parameters and returns their product.
+"""
+
 
 # Adds 2 numbers, returns sum
 def add(num_1, num_2):
     return num_1 + num_2
+
+
+# multiplies 2 numbers, returns result
+def multiply(num_1, num_2):
+    return num_1 * num_2
