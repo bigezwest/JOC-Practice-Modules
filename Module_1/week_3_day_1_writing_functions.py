87f10b67c2795e778eef94e31c64eb100882981e
Thomas DAngelo <tommy@languagetribe.com>
2023-07-21 21:42:09 -0500

    Initial Commit & push. Created add function.

---
+++ Module_1/week_3_day_1_writing_functions.py
@@ -0,0 +1,7 @@
+# Thomas D'Angelo
+# 1. Write a function, add, that takes two numbers as parameters and
+# returns their sum.
+
+# Adds 2 numbers, returns sum
+def add(num_1, num_2):
+    return num_1 + num_2
