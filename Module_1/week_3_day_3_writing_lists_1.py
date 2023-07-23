f4df768bf04dc234c5ec77d9c2f3811395cab2ae
Thomas DAngelo <tommy@languagetribe.com>
2023-07-23 09:47:41 -0500

    Renamed list.  Refactored averaging to use built-in functions.

--- Module_1/week_3_day_3_writing_lists_1.py
+++ Module_1/week_3_day_3_writing_lists_1.py
@@ -7,18 +7,11 @@
 
 # Create a list of 20 numbers, input by user.
 def writeAList():
-    my_list = []
+    list = []
     for i in range(20):
         user_num = int(input("Enter a number: "))
-        my_list.append(user_num)
-
-    total = 0                       # Accumulator for total
-    length = 0                      # To hold the size of the list
-    for item in (my_list):
-        total += item               # Add to total
-        length += 1                 # Increment length of list
-    avg = total / length            # Calculate the average
-    print("The average of the numbers is", avg)
+        list.append(user_num)
+    print("Average: ", sum(list) / len(list))
 
 def main():
     writeAList()
