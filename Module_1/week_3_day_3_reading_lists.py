b886626f02556439456517f068ddb061a29d374b
Thomas DAngelo <tommy@languagetribe.com>
2023-07-23 07:30:21 -0500

    Updated print formatting.

--- Module_1/week_3_day_3_reading_lists.py
+++ Module_1/week_3_day_3_reading_lists.py
@@ -3,40 +3,44 @@
 # What does this program output?
 
 my_list = ["one", "two", "three", "four", "five"]
-print("\t------------------------------------------------------")
-print("\tTEST 1")
-print("\n\tTrue indicates that the result is what was expected")
-print("\tTest\t\t\t Result")
-print("\t----------\t\t -----------")
-print("\tmy_list[0]\t\t", my_list[0] == "one")
-print("\tmy_list[-1]\t\t", my_list[-1] == "five")
-print("\tmy_list[2:4]\t", my_list[2:4] == ["three", "four"])
-print("\t------------------------------------------------------")
+print("------------------------------------------------------")
+print("TEST 1")
+print("\nTrue indicates that the result is what was expected")
+print("Test\t\t\t Result")
+print("----------\t\t -----------")
+print("my_list[0]\t\t", my_list[0] == "one")
+print("my_list[-1]\t\t", my_list[-1] == "five")
+print("my_list[2:4]\t", my_list[2:4] == ["three", "four"])
+print("------------------------------------------------------")
 
-print("\n\tTEST 2")
-print("\t-------")
+print("\nTEST 2")
+print("-------")
 for num in my_list:
     if num.count("o") > 0:
-        print("\t", num)
+        print(num)
 
-print("\t------------------------------------------------------")
+print("------------------------------------------------------")
 
-print("\n\tTEST 3")
-print("\t______")
+print("\nTEST 3")
+print("______")
 phrase = "Green Eggs & Ham"
 print(("green" in phrase) == False)
 print(("Green" in phrase) == True)
 print(("two" in phrase) == False)
 
-print("\t------------------------------------------------------")
-print("\tTEST 4")
+print("------------------------------------------------------")
+print("\nTEST 4")
+print("______")
+
 phrase = "Monty Python"
 for letter in phrase:
     print(letter, end="-")
 print()
 
-print("\t------------------------------------------------------")
-print("\tTEST 1")
+print("------------------------------------------------------")
+print("\nTEST 5")
+print("______")
+
 # Assume ythe user enters the following numbers:
 # 5 10 15 20 25
 numbers = []
