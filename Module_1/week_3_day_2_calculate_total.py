805d718d872078a80d6f1c118c7658316a31055d
Thomas DAngelo <tommy@languagetribe.com>
2023-07-23 06:09:02 -0500

    Updated Comments. Separated tip & tax in calculate_total().   Updated print statements.

--- Module_1/week_3_day_2_calculate_total.py
+++ Module_1/week_3_day_2_calculate_total.py
@@ -1,3 +1,6 @@
+# calculate_total
+# Thomas D'Angelo
+#
 # Write a function calcualte_total that takes 3 parameters:
 #   a. meal: the cost of the meal.
 #   b. tax_rate: The percent tax (e.g., NJ tax would be 0.07)
@@ -8,13 +11,16 @@
 # Note: that the total for the above meal with tax & tip is $66.85.
 
 def calculate_total(meal, tax_rate, tip_rate):
-    return meal + (tax_rate * meal) + (tip_rate * meal)
+    tip = tip_rate * meal
+    tax = tax_rate * meal
+
+    return meal + tip + tax
 
 
 def main():
 
-    print("\n\tResult calling: calculate_total(53.48, .07, .18)")
-    print("\tThe total is $", calculate_total(53.48, .07, .18), sep="")
+    print("\n\tCalling: calculate_total(53.48, .07, .18)")
+    print("\tThe total should be $66.85: $", calculate_total(53.48, .07, .18), sep="")
 
 
 main()
