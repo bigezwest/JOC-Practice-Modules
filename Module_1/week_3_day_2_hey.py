60f49f44560673fbd9c4df6c272f7699c2152b7b
Thomas DAngelo <tommy@languagetribe.com>
2023-07-23 06:25:39 -0500

    Updated Comments. Updated print statements.

--- Module_1/week_3_day_2_hey.py
+++ Module_1/week_3_day_2_hey.py
@@ -1,3 +1,6 @@
+# hey
+# Thomas D'Angelo
+#
 # 6. Write a function hey that takes a number as a parameter, squares it, and outputs the parameter squared:
 # Example call Returns
 #   hey(5) 25
@@ -11,9 +14,12 @@ def hey(num_1):
 
 # Calls and prints hey() to show the square of number
 def main():
-    print("5 squared is ", hey(5))      # Positive test
-    print("0 squared is", hey(0))       # 0 test
-    print("-3 squared is", hey(-3))     # Negative test
-
+    print('\t---------------------------------------------------')
+    print('\tTest\t\t\t\t\t Function Call\t\t Result')
+    print('\t----\t\t\t\t\t -------------\t\t ------')
+    print("\tPostive Number Test:\t Calling hey(5)\t\t", hey(5) == 25)      # Positive test
+    print("\tZero Test:\t\t\t\t Calling hey(0)\t\t", hey(0) == 0)           # 0 test
+    print("\tNegative Number Test:\t Calling hey(-3)\t", hey(-3) == 9)      # Negative test
+    print('\t---------------------------------------------------')
 
 main()
