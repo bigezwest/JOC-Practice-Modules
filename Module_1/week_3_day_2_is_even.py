db19e52c77f01b24ab56d819ee8b310064a44366
Thomas DAngelo <tommy@languagetribe.com>
2023-07-23 06:02:01 -0500

    Updated comments. Updated is_even() to be more concise. Updated output statements.

--- Module_1/week_3_day_2_is_even.py
+++ Module_1/week_3_day_2_is_even.py
@@ -1,17 +1,18 @@
+# Writing is_even
+# Thomas D'Angelo
+#
 # Write a function is_even that takes a number as a parameter and returns whether or not it is even.
 # Test that your function works by calling it twice, once with an even number and once with an odd
 # number, and print the results.
 
+# Tests if a number is even.  Boolean return
 def is_even(num):
-    if num % 2 == 0:
-        return True
-    else:
-        return False
+    return num % 2 == 0
 
 
 def main():
-    print (is_even(6))
-    print(is_even(3))
+    print("4 is even?", is_even(6))
+    print("3 is even?", is_even(3))
 
 
 main()
