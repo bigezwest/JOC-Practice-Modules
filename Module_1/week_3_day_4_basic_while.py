1a922285c03d6e1560239db4b740506e91ca0e79
Thomas DAngelo <tommy@languagetribe.com>
2023-07-24 10:49:12 -0500

    func_c, func_2a, func_2b, func_2c added

--- Module_1/week_3_day_4_basic_while.py
+++ Module_1/week_3_day_4_basic_while.py
@@ -18,6 +18,53 @@ def func_b():
     print(string, "->", builder)
 
 
+def func_c():
+    x = 0
+    i = 1
+    while x < 20:
+        if x > 5:
+            x += 15
+        else:
+            x += 1
+        print(i, x)
+        i += 1
+
+
+def func_2a():
+    string = "HELLO"
+    x = 0
+    while string[x] != 'L':
+        print(string[x], end="... ")
+        x += 1
+    print("\n", string, "first L is at", x)
+
+
+def func_2b():
+    # Assume the user enters the following:
+    # hellow goodbye cat dog DONE done
+    list = []
+    prompt = "Please enter word, 'done' to finish "
+    response = input(prompt)
+    while response != "done":
+        list.append(response)
+        response = input(prompt)
+    print(sorted(list))
+
+
+def func_2c():
+    x = 0
+    while x < 20:
+        if x > 5:
+            if x % 2 == 0:
+                x *= 2
+            else:
+                x *= -1
+        else:
+            x += 10
+        x += 1
+    print(x)
+
+
 # Testing each function
 def main():
     print("--------------- ")
@@ -28,6 +75,21 @@ def main():
     print("Running func_b ")
     func_b()
 
+    print("--------------- ")
+    print("Running func_c ")
+    func_c()
+
+    print("--------------- ")
+    print("Running func_2a ")
+    func_2a()
+
+    print("--------------- ")
+    print("Running func_2b ")
+    func_2b()
+
+    print("--------------- ")
+    print("Running func_2c ")
+    func_2c()
 
 
 main()
